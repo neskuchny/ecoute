2242fb72cfacfdb9141279d60bbf6bb89e49f499
neskuchny <89947070+neskuchny@users.noreply.github.com>
2023-06-01 00:14:31 +0300

    Update prompts.py

--- prompts.py
+++ prompts.py
@@ -5,3 +5,4 @@ def create_prompt(transcript):
 {transcript}.
 
 Please respond, in detail, to the conversation. Confidently give a straightforward response to the speaker, even if you don't understand them. Give your response in square brackets. DO NOT ask to repeat, and DO NOT ask for clarification. Just answer the speaker directly."""
+#translate in to russia
